d5415abc7b2bb0ac2c61cd3e2b3a548bbea93662
HaeckelK <HaeckelK@gmail.com>
2022-02-10 22:56:55 +0000

    feat: first attempt at GenerateCode using custom type Argument

--- python-argparse-generator/src/index.ts
+++ python-argparse-generator/src/index.ts
@@ -1,1 +1,65 @@
 export const Greeter = (name: string) => `Hello ${name}`;
+
+type Argument = {
+  name: string;
+  type: string;
+  variableName: string;
+  default: string;
+  defaultDisplay: string;
+};
+
+function argumentToText(argument: Argument) {
+  switch (argument.type) {
+    case 'bool':
+      return `parser.add_argument("${argument.name}", action="store_${argument.default}")`;
+      break;
+    default:
+      if (argument.default === undefined) {
+        return `parser.add_argument("${argument.name}", type=${argument.type})`;
+      } else {
+        return `parser.add_argument("${argument.name}", type=${argument.type}, default=${argument.defaultDisplay})`;
+      }
+  }
+}
+
+const argumentToMainParams = (argument: Argument) => `${argument.variableName}: ${argument.type}`;
+
+export const GenerateCode = (args: Argument[]) => {
+  let mainParameters: string[] = args.map((arg) => argumentToMainParams(arg));
+  let argumentsText: string[] = args.map((arg) => argumentToText(arg));
+  let returnText: string[] = args.map((x) => {
+    if (x.type === 'str') {
+      return `"${x.variableName}": args.${x.variableName},`;
+    } else {
+      return `"${x.variableName}": ${x.type}(args.${x.variableName}),`;
+    }
+  });
+
+  let output = `import argparse
+from typing import Dict, Any
+  
+def main(${mainParameters.join(', ')}) -> None:
+  # Contents of main
+  return
+
+
+def cli() -> Dict[str, Any]:
+  formatter_class = argparse.ArgumentDefaultsHelpFormatter
+  parser = argparse.ArgumentParser(formatter_class=formatter_class)
+
+  ${argumentsText.join('\n    ')}
+
+  args = parser.parse_args()
+
+  return {${returnText.join('\n            ').slice(0, -1)}}
+
+if __name__ == '__main__':
+  args = cli()
+  main(${args.map((x) => `${x.variableName}=args["${x.variableName}"]`).join(', ')})
+`;
+  return output;
+};
+
+const argument: Argument = { name: 'folder', type: 'str', variableName: 'folder', default: '', defaultDisplay: '' };
+
+console.log(GenerateCode([argument]));
