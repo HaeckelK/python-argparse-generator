b74bccf0f4ec5630c884e11dea387e5316a1bbb2
HaeckelK <51128712+HaeckelK@users.noreply.github.com>
2022-02-11 09:08:17 +0000

    feat: draft code for GenerateCode function (#2)
    * feat: first attempt at GenerateCode using custom type Argument
    
    * refactor: linting and drop demo
    
    Co-authored-by: HaeckelK <HaeckelK@gmail.com>

--- python-argparse-generator/src/index.ts
+++ python-argparse-generator/src/index.ts
@@ -1,1 +1,61 @@
 export const Greeter = (name: string) => `Hello ${name}`;
+
+export type Argument = {
+  name: string;
+  type: string;
+  variableName: string;
+  default: string;
+  defaultDisplay: string;
+};
+
+function argumentToText(argument: Argument) {
+  switch (argument.type) {
+    case 'bool':
+      return `parser.add_argument("${argument.name}", action="store_${argument.default}")`;
+      break;
+    default:
+      if (argument.default === undefined) {
+        return `parser.add_argument("${argument.name}", type=${argument.type})`;
+      } else {
+        return `parser.add_argument("${argument.name}", type=${argument.type}, default=${argument.defaultDisplay})`;
+      }
+  }
+}
+
+const argumentToMainParams = (argument: Argument) => `${argument.variableName}: ${argument.type}`;
+
+export const GenerateCode = (args: Argument[]) => {
+  const mainParameters: string[] = args.map((arg) => argumentToMainParams(arg));
+  const argumentsText: string[] = args.map((arg) => argumentToText(arg));
+  const returnText: string[] = args.map((x) => {
+    if (x.type === 'str') {
+      return `"${x.variableName}": args.${x.variableName},`;
+    } else {
+      return `"${x.variableName}": ${x.type}(args.${x.variableName}),`;
+    }
+  });
+
+  const output = `import argparse
+from typing import Dict, Any
+  
+def main(${mainParameters.join(', ')}) -> None:
+  # Contents of main
+  return
+
+
+def cli() -> Dict[str, Any]:
+  formatter_class = argparse.ArgumentDefaultsHelpFormatter
+  parser = argparse.ArgumentParser(formatter_class=formatter_class)
+
+  ${argumentsText.join('\n    ')}
+
+  args = parser.parse_args()
+
+  return {${returnText.join('\n            ').slice(0, -1)}}
+
+if __name__ == '__main__':
+  args = cli()
+  main(${args.map((x) => `${x.variableName}=args["${x.variableName}"]`).join(', ')})
+`;
+  return output;
+};
