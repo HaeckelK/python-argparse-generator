648b8c694225df1c8a284873fb4809113a7388d9
HaeckelK <51128712+HaeckelK@users.noreply.github.com>
2022-02-15 18:10:47 +0000

    Reduce needed params for Argument, and create Settings object for argparseCode (#9)
    * feat: add required boolean field to Argument
    
    * refactor: remove defaultDisplay from Argument
    
    * refactor: GenerateCode -> argparseCode
    
    * feat: parserName argument
    
    * feat: parserName default
    
    * feat: format main call vertically
    
    * feat: newArgument
    
    * feat: Settings type
    
    * feat: mainContents
    
    * feat: mainName
    
    * feat: cliName
    
    * feat: argsName to settings
    
    * feat: add newlines
    
    * feat: formatting
    
    * feat: toggle type hints
    
    * style: formatting
    
    Co-authored-by: HaeckelK <haeckelk.github@gmail.com>

--- python-argparse-generator/src/__tests__/index.test.ts
+++ python-argparse-generator/src/__tests__/index.test.ts
@@ -1,14 +1,32 @@
-import { GenerateCode } from '../index';
+import { argparseCode, newArgument, defaultSettings } from '../index';
 
-test('Basic check of output', () => {
-  expect(
-    GenerateCode([
-      { name: 'folder', type: 'str', variableName: 'folder', default: '"/data"', defaultDisplay: '"/data"' },
-      { name: 'limit', type: 'int', variableName: 'limit', default: '10', defaultDisplay: '10' },
-    ]),
-  ).toBe(`import argparse
+describe('Check Of argparseCode Output', () => {
+  const args = [
+    {
+      name: 'folder',
+      type: 'str',
+      variableName: 'folder',
+      default: '/data',
+      required: true,
+    },
+    { name: 'limit', type: 'int', variableName: 'limit', default: '10', required: true },
+  ];
+  test('Basic check of output', () => {
+    expect(
+      argparseCode([
+        {
+          name: 'folder',
+          type: 'str',
+          variableName: 'folder',
+          default: '/data',
+          required: true,
+        },
+        { name: 'limit', type: 'int', variableName: 'limit', default: '10', required: true },
+      ]),
+    ).toBe(`import argparse
 from typing import Dict, Any
-  
+
+
 def main(folder: str, limit: int) -> None:
     # Contents of main
     return
@@ -26,8 +44,72 @@ def cli() -> Dict[str, Any]:
     return {"folder": args.folder,
             "limit": int(args.limit)}
 
+
 if __name__ == '__main__':
     args = cli()
-    main(folder=args["folder"], limit=args["limit"])
+    main(folder=args["folder"],
+         limit=args["limit"])
 `);
+  });
+  test('Type Hints False', () => {
+    let settings = defaultSettings();
+    settings.typeHints = false;
+    expect(argparseCode(args, settings)).toBe(`import argparse
+
+
+def main(folder, limit):
+    # Contents of main
+    return
+
+
+def cli():
+    formatter_class = argparse.ArgumentDefaultsHelpFormatter
+    parser = argparse.ArgumentParser(formatter_class=formatter_class)
+
+    parser.add_argument("folder", type=str, default="/data")
+    parser.add_argument("limit", type=int, default=10)
+
+    args = parser.parse_args()
+
+    return {"folder": args.folder,
+            "limit": int(args.limit)}
+
+
+if __name__ == '__main__':
+    args = cli()
+    main(folder=args["folder"],
+         limit=args["limit"])
+`);
+  });
+});
+
+describe('New Argument Factory', () => {
+  test('Default Parameters', () => {
+    expect(newArgument('folder', 'str')).toStrictEqual({
+      name: 'folder',
+      type: 'str',
+      default: '',
+      variableName: 'folder',
+      required: false,
+    });
+  });
+  // TODO really I should have a variableNameCreate function
+  test('Variable Name Creation', () => {
+    expect(newArgument('time-delay', 'str')).toStrictEqual({
+      name: 'time-delay',
+      type: 'str',
+      default: '',
+      variableName: 'timedelay',
+      required: false,
+    });
+  });
+  test('All Params', () => {
+    expect(newArgument('folder', 'str', 'folder', '/data', true)).toStrictEqual({
+      name: 'folder',
+      type: 'str',
+      default: '/data',
+      variableName: 'folder',
+      required: true,
+    });
+  });
 });
